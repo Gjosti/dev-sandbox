f945780869e14ca163c84f8e03f7b4251ee48d03
Gjosti <stian_943@hotmail.com>
2025-09-27 19:38:22 +0200

    more crouch.

--- Player/crouch.gd
+++ Player/crouch.gd
@@ -1,7 +1,10 @@
 extends Node3D
 
 @export var player: Player
-@export var crouch_pitch: float = -45.0 # Degrees to lean forward
+@export var crouch_mesh_scale: Vector3 = Vector3(1, 0.5, 1)
+@export var stand_mesh_scale: Vector3 = Vector3(1, 1, 1)
+@export var crouch_height: float = 1.0
+@export var stand_height: float = 2.0
 
 var is_crouching: bool = false
 
@@ -9,11 +12,16 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("crouch"):
 		if not is_crouching:
 			is_crouching = true
-			var pitch_rad = deg_to_rad(crouch_pitch)
-			player.player_mesh.rotation.x = pitch_rad
-			player.collision_shape_3d.rotation.x = pitch_rad
-	else:
+			player.player_mesh.scale = crouch_mesh_scale
+			var capsule := player.collision_shape_3d.shape as CapsuleShape3D
+			player.collision_shape_3d.position.y = 0.5
+			capsule.height = crouch_height
+			player.rig.travel("Crouch")
+	elif event.is_action_released("crouch"):
 		if is_crouching:
 			is_crouching = false
-			player.player_mesh.rotation.x = 0.0
-			player.collision_shape_3d.rotation.x = 0.0
+			player.player_mesh.scale = stand_mesh_scale
+			var capsule := player.collision_shape_3d.shape as CapsuleShape3D
+			capsule.height = stand_height
+			player.collision_shape_3d.position.y = 1
+			player.rig.travel("MoveSpace")
