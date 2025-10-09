9485941eb8f35980a73bd8fc5bd2316fa15eea67
Gjosti <stian_943@hotmail.com>
2025-10-09 15:22:45 +0200

    simplified crouch, removed slide logic for now

--- Player/crouch.gd
+++ Player/crouch.gd
@@ -1,22 +1,19 @@
-extends Node3D
+extends Node
 
 @export var player: Player
-@export_group("Slide Properties")
-@export var slide_cooldown: float = 0.5
-@export var time_remaining: float = 0.0
 
 var stand_mesh_scale: Vector3 = Vector3(1, 1, 1)
 var stand_height: float = 2.0
 var crouch_mesh_scale: Vector3 = Vector3(1, 0.5, 1)
 var crouch_height: float = 1.0
 
+
 func _unhandled_input(event: InputEvent) -> void:
 	# Simple crouching
 	if event.is_action_pressed("crouch"):
 		crouch()
 	elif event.is_action_released("crouch"):
-		if player.rig.is_sliding() or player.rig.is_crouching():
-			stand()
+		stand()
 
 func crouch() -> void:
 	player.player_mesh.scale = crouch_mesh_scale
@@ -26,9 +23,10 @@ func crouch() -> void:
 	player.rig.travel("Crouch")
 
 func stand() -> void:
-	player.player_mesh.scale = stand_mesh_scale
-	var capsule := player.collision_shape_3d.shape as CapsuleShape3D
-	capsule.height = stand_height
-	player.collision_shape_3d.position.y = 1
-	player.rig.travel("MoveSpace")
+	if player.rig.is_crouching():
+		player.player_mesh.scale = stand_mesh_scale
+		var capsule := player.collision_shape_3d.shape as CapsuleShape3D
+		capsule.height = stand_height
+		player.collision_shape_3d.position.y = 1
+		player.rig.travel("MoveSpace")
 
