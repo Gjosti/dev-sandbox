c16b4a87709690da6751936d4afe47c4ba18b38a
Gjosti <stian_943@hotmail.com>
2025-10-09 15:37:11 +0200

    cleanup

--- Player/jump.gd
+++ Player/jump.gd
@@ -14,12 +14,6 @@ extends Node3D
 var coyote_time: float = 0.1
 var coyote_timer: float = 0.0
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if player.is_on_floor():
 		jumps_left = extra_jumps
