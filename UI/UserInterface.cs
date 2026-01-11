fe2d8566d870b77bedfbf0e36b1035582479eb46
Gjosti <stian_943@hotmail.com>
2026-01-11 20:44:45 +0100

    basic setup, more to come

--- UI/UserInterface.cs
+++ UI/UserInterface.cs
@@ -3,28 +3,28 @@ using System;
 
 public partial class UserInterface : Control
 {
-    [Export] public Player Player { get; set; }
-    
-    private Label _velocityLabel;
+	[Export] public Player Player { get; set; }
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        _velocityLabel = GetNode<Label>("%VelocityLabel");
-        
-        if (Player != null)
-        {
-            Player.VelocityCurrent += OnPlayerVelocityCurrent;
-        }
-    }
+	private Label _velocityLabel;
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_velocityLabel = GetNode<Label>("%VelocityLabel");
 
-    private void OnPlayerVelocityCurrent(Vector3 currentVelocity)
-    {
-        _velocityLabel.Text = (Mathf.Round(currentVelocity.Length() * 10) / 10).ToString();
-    }
+		if (Player != null)
+		{
+			Player.VelocityCurrent += OnPlayerVelocityCurrent;
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void OnPlayerVelocityCurrent(Vector3 currentVelocity)
+	{
+		_velocityLabel.Text = (Mathf.Round(currentVelocity.Length() * 10) / 10).ToString();
+	}
 }
