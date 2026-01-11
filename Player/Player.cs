ed555baa4098d5fd84c01e3b5d4b82ac294ae7ce
Gjosti <stian_943@hotmail.com>
2026-01-11 22:23:29 +0100

    Player can now apply force to RigidBody3D/movable props

--- Player/Player.cs
+++ Player/Player.cs
@@ -1,180 +1,210 @@
 using Godot;
 
+
+// Additional movement and abilities are added as separate nodes under this node/class.
+// Remember to attach this node to subnodes mentioned above.
 public partial class Player : CharacterBody3D
 {
-    [Signal]
-    public delegate void VelocityCurrentEventHandler(Vector3 currentVelocity);
+	[Signal]
+	public delegate void VelocityCurrentEventHandler(Vector3 currentVelocity);
 
-    [ExportGroup("Movement Settings")]
-    [Export] public float MovementSpeed { get; set; } = 6.0f;
-    [Export] public float CrouchMovementModifier { get; set; }
-    [Export] public float Acceleration { get; set; } = 30f;
-    [Export] public float AirAcceleration { get; set; } = 10.0f;
-    [Export] public float AirDrag { get; set; } = 0.5f;
-    [Export] public float GroundFriction { get; set; } = 200f;
+	[ExportGroup("Movement Settings")]
+	[Export] public float MovementSpeed { get; set; } = 6.0f;
+	[Export] public float PushForce = 60f;
+	[Export] public float CrouchMovementModifier { get; set; }
+	[Export] public float Acceleration { get; set; } = 30f;
+	[Export] public float AirAcceleration { get; set; } = 10.0f;
+	[Export] public float AirDrag { get; set; } = 0.5f;
+	[Export] public float GroundFriction { get; set; } = 200f;
 
-    public float AirTurnFaceRate = 10f;
-    public float GroundTurnRate = 20f;
+	public float AirTurnFaceRate = 10f;
+	public float GroundTurnRate = 20f;
 
-    [ExportGroup("Camera Settings")]
-    [Export] public float MouseSensitivity { get; set; } = 0.00075f;
-    [Export] public float CameraScrollSensitivity { get; set; } = 0.25f;
-    [Export] public float MinCameraRotation { get; set; } = -90f;
-    [Export] public float MaxCameraRotation { get; set; } = 90f;
-    [Export] public float MinCameraDistance { get; set; } = 1f;
-    [Export] public float MaxCameraDistance { get; set; } = 15f;
+	[ExportGroup("Camera Settings")]
+	[Export] public float MouseSensitivity { get; set; } = 0.00075f;
+	[Export] public float CameraScrollSensitivity { get; set; } = 0.25f;
+	[Export] public float MinCameraRotation { get; set; } = -90f;
+	[Export] public float MaxCameraRotation { get; set; } = 90f;
+	[Export] public float MinCameraDistance { get; set; } = 1f;
+	[Export] public float MaxCameraDistance { get; set; } = 15f;
 
-    public Vector3 Direction { get; set; } = Vector3.Zero;
-    private Vector3 _horizontalVelocity = Vector3.Zero;
+	public Vector3 Direction { get; set; } = Vector3.Zero;
+	private Vector3 _horizontalVelocity = Vector3.Zero;
 
-    public Node3D PlayerMesh { get; private set; }
-    public CollisionShape3D CollisionShape { get; private set; }
-    public Node3D RigPivot { get; private set; }
-    public Rig Rig { get; private set; }
-    private Jump _jump;
-    private PlayerCamera _camera;
+	public Node3D PlayerMesh { get; private set; }
+	public CollisionShape3D CollisionShape { get; private set; }
+	public Node3D RigPivot { get; private set; }
+	public Rig Rig { get; private set; }
+	private Jump _jump;
+	private PlayerCamera _camera;
 
-    public override void _Ready()
-    {
-        PlayerMesh = GetNode<Node3D>("RigPivot/Rig/CharacterRig/MeshInstance3D");
-        CollisionShape = GetNode<CollisionShape3D>("CollisionShape3D");
-        RigPivot = GetNode<Node3D>("RigPivot");
-        Rig = GetNode<Rig>("RigPivot/Rig");
-        _jump = GetNode<Jump>("Jump");
-        _camera = GetNode<PlayerCamera>("PlayerCamera");
-    }
+	public override void _Ready()
+	{
+		PlayerMesh = GetNode<Node3D>("RigPivot/Rig/CharacterRig/MeshInstance3D");
+		CollisionShape = GetNode<CollisionShape3D>("CollisionShape3D");
+		RigPivot = GetNode<Node3D>("RigPivot");
+		Rig = GetNode<Rig>("RigPivot/Rig");
+		_jump = GetNode<Jump>("Jump");
+		_camera = GetNode<PlayerCamera>("PlayerCamera");
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        ApplyGravity((float)delta);
-        HandleMovement((float)delta);
-        MoveAndSlide();
+	public override void _PhysicsProcess(double delta)
+	{
+		ApplyGravity((float)delta);
+		HandleMovement((float)delta);
+		MoveAndSlide();
 
-        EmitSignal(SignalName.VelocityCurrent, Velocity);
-    }
+		// Push rigid bodies after movement
+		ApplyPushForce();
 
-    private void ApplyGravity(float delta)
-    {
-        if (Rig.IsDashing())
-        {
-            Velocity = new Vector3(Velocity.X, 0, Velocity.Z);
-        }
-        else
-        {
-            Velocity = new Vector3(Velocity.X, Velocity.Y + GetPlayerGravity() * delta, Velocity.Z);
-        }
-    }
+		EmitSignal(SignalName.VelocityCurrent, Velocity);
+	}
 
-    public float GetPlayerGravity()
-    {
-        if (_jump != null)
-        {
-            return _jump.GetGravity(Velocity.Y);
-        }
-        else
-        {
-            GD.PrintErr("Using project settings gravity and not jump gravity!");
-            return ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
-        }
-    }
+	private void ApplyGravity(float delta)
+	{
+		if (Rig.IsDashing())
+		{
+			Velocity = new Vector3(Velocity.X, 0, Velocity.Z);
+		}
+		else
+		{
+			Velocity = new Vector3(Velocity.X, Velocity.Y + GetPlayerGravity() * delta, Velocity.Z);
+		}
+	}
 
-    private Vector3 GetCameraMovementDirection()
-    {
-        Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
-        if (inputDir == Vector2.Zero)
-            return Vector3.Zero;
+	public float GetPlayerGravity()
+	{
+		if (_jump != null)
+		{
+			return _jump.GetGravity(Velocity.Y);
+		}
+		else
+		{
+			GD.PrintErr("Using project settings gravity and not jump gravity!");
+			return ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+		}
+	}
 
-        Basis cameraBasis = new Basis(Vector3.Up, _camera.HorizontalPivot.Rotation.Y);
-        return (cameraBasis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-    }
+	private Vector3 GetCameraMovementDirection()
+	{
+		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
+		if (inputDir == Vector2.Zero)
+			return Vector3.Zero;
 
-    private void HandleMovement(float delta)
-    {
-        if (Rig.IsDashing() || Rig.IsSliding())
-            return;
+		Basis cameraBasis = new Basis(Vector3.Up, _camera.HorizontalPivot.Rotation.Y);
+		return (cameraBasis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+	}
 
-        Direction = GetCameraMovementDirection();
-        _horizontalVelocity = GetHorizontalVelocity();
+	private void HandleMovement(float delta)
+	{
+		if (Rig.IsDashing() || Rig.IsSliding())
+			return;
 
-        if (IsOnFloor())
-        {
-            _horizontalVelocity = ApplyGroundMovement(_horizontalVelocity, delta);
-        }
-        else
-        {
-            _horizontalVelocity = ApplyAirMovement(_horizontalVelocity, delta);
-        }
+		Direction = GetCameraMovementDirection();
+		_horizontalVelocity = GetHorizontalVelocity();
 
-        SetHorizontalVelocity(_horizontalVelocity);
-        Rig.UpdateAnimationTree(Direction);
-    }
+		if (IsOnFloor())
+		{
+			_horizontalVelocity = ApplyGroundMovement(_horizontalVelocity, delta);
+		}
+		else
+		{
+			_horizontalVelocity = ApplyAirMovement(_horizontalVelocity, delta);
+		}
 
-    private Vector3 GetHorizontalVelocity()
-    {
-        return new Vector3(Velocity.X, 0, Velocity.Z);
-    }
+		SetHorizontalVelocity(_horizontalVelocity);
+		Rig.UpdateAnimationTree(Direction);
+	}
 
-    private void SetHorizontalVelocity(Vector3 horizontal)
-    {
-        Velocity = new Vector3(horizontal.X, Velocity.Y, horizontal.Z);
-    }
+	private Vector3 GetHorizontalVelocity()
+	{
+		return new Vector3(Velocity.X, 0, Velocity.Z);
+	}
 
-    private void FaceDirection(float rate, float delta)
-    {
-        if (Direction == Vector3.Zero)
-            return;
+	private void SetHorizontalVelocity(Vector3 horizontal)
+	{
+		Velocity = new Vector3(horizontal.X, Velocity.Y, horizontal.Z);
+	}
 
-        float targetYaw = Mathf.Atan2(-Direction.X, -Direction.Z);
-        RigPivot.Rotation = new Vector3(
-            RigPivot.Rotation.X,
-            Mathf.LerpAngle(RigPivot.Rotation.Y, targetYaw, rate * delta),
-            RigPivot.Rotation.Z);
-    }
+	private void FaceDirection(float rate, float delta)
+	{
+		if (Direction == Vector3.Zero)
+			return;
 
-    private Vector3 ApplyGroundMovement(Vector3 horizontal, float delta)
-    {
-        if (Direction != Vector3.Zero)
-        {
-            Vector3 target = Direction * MovementSpeed;
-            horizontal = horizontal.MoveToward(target, Acceleration * delta);
-            FaceDirection(GroundTurnRate, delta);
-        }
-        else
-        {
-            horizontal = horizontal.MoveToward(Vector3.Zero, GroundFriction * delta);
-        }
-        return horizontal;
-    }
+		float targetYaw = Mathf.Atan2(-Direction.X, -Direction.Z);
+		RigPivot.Rotation = new Vector3(
+			RigPivot.Rotation.X,
+			Mathf.LerpAngle(RigPivot.Rotation.Y, targetYaw, rate * delta),
+			RigPivot.Rotation.Z);
+	}
 
-    private Vector3 ApplyAirMovement(Vector3 horizontal, float delta)
-    {
-        horizontal -= horizontal * AirDrag * delta;
-        if (Direction != Vector3.Zero)
-        {
-            horizontal += Direction * AirAcceleration * delta;
-            FaceDirection(AirTurnFaceRate, delta);
-        }
-        return horizontal;
-    }
+	private Vector3 ApplyGroundMovement(Vector3 horizontal, float delta)
+	{
+		if (Direction != Vector3.Zero)
+		{
+			Vector3 target = Direction * MovementSpeed;
+			horizontal = horizontal.MoveToward(target, Acceleration * delta);
+			FaceDirection(GroundTurnRate, delta);
+		}
+		else
+		{
+			horizontal = horizontal.MoveToward(Vector3.Zero, GroundFriction * delta);
+		}
+		return horizontal;
+	}
 
-    public override void _UnhandledInput(InputEvent @event)
-    {
-        HandleAttackInput(@event);
-    }
+	private Vector3 ApplyAirMovement(Vector3 horizontal, float delta)
+	{
+		horizontal -= horizontal * AirDrag * delta;
+		if (Direction != Vector3.Zero)
+		{
+			horizontal += Direction * AirAcceleration * delta;
+			FaceDirection(AirTurnFaceRate, delta);
+		}
+		return horizontal;
+	}
 
-    private void HandleAttackInput(InputEvent @event)
-    {
-        if (Rig.IsIdle())
-        {
-            if (@event.IsActionPressed("attack"))
-            {
-                MainAction();
-            }
-        }
-    }
+	private void ApplyPushForce()
+	{
+		for (int i = 0; i < GetSlideCollisionCount(); i++)
+		{
+			KinematicCollision3D collision = GetSlideCollision(i);
+			if (collision.GetCollider() is RigidBody3D rigidBody)
+			{
+				Vector3 normal = collision.GetNormal();
 
-    private void MainAction()
-    {
-        Rig.Travel("Attack");
-    }
+				// Only push if the collision is more horizontal than vertical
+				// If normal.Y is close to 1, we're standing on top - don't push
+				// If normal.Y is close to 0, it's a side collision - push it
+				// Only push if collision is less than 60° from horizontal Lower values (e.g., 0.3f) = only push nearly horizontal collisions
+				// Higher values (e.g., 0.7f) = push more collisions, even at steeper angles
+				if (Mathf.Abs(normal.Y) < 0.5f)
+				{
+					Vector3 impulse = -normal * PushForce;
+					rigidBody.ApplyCentralImpulse(impulse);
+				}
+			}
+		}
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		HandleAttackInput(@event);
+	}
+
+	private void HandleAttackInput(InputEvent @event)
+	{
+		if (Rig.IsIdle())
+		{
+			if (@event.IsActionPressed("attack"))
+			{
+				MainAction();
+			}
+		}
+	}
+
+	private void MainAction()
+	{
+		Rig.Travel("Attack");
+	}
 }
