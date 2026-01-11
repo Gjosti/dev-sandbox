23983885326cc11d101ddf871f83da06fc5aef7d
Gjosti <stian_943@hotmail.com>
2026-01-11 15:19:22 +0100

    Crouch script converted to C#

---
+++ Player/Crouch.cs
@@ -0,0 +1,147 @@
+using Godot;
+
+public partial class Crouch : Node
+{
+	[Export] public CharacterBody3D Player { get; set; }
+	[Export] public float SlideThreshold { get; set; } = 6.1f;
+	[Export] public float SlideMinThreshold { get; set; } = 3f;
+	[Export] public float SlideFriction { get; set; } = 0.985f;
+	[Export] public float SlideTurnRate { get; set; } = 0.05f;
+
+	private readonly Vector3 _standMeshScale = new(1, 1, 1);
+	private const float StandHeight = 2.0f;
+	private readonly Vector3 _crouchMeshScale = new(1, 0.5f, 1);
+	private const float CrouchHeight = 1.0f;
+	private Vector3 _velocity = Vector3.Zero;
+
+	private bool _rotatePlayerLeft = false;
+	private bool _rotatePlayerRight = false;
+
+	private Rig _rig;
+	private Node3D _rigPivot;
+	private Node3D _playerMesh;
+	private CollisionShape3D _collisionShape;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		if (_rig == null && Player != null)
+		{
+			_rig = Player.GetNode<Rig>("RigPivot/Rig");
+			_rigPivot = Player.GetNode<Node3D>("RigPivot");
+			_playerMesh = Player.GetNode<Node3D>("RigPivot/Rig/CharacterRig/MeshInstance3D");
+			_collisionShape = Player.GetNode<CollisionShape3D>("CollisionShape3D");
+		}
+
+		if (_rig.IsSliding())
+		{
+			ApplySimpleSlide((float)delta);
+			if (_velocity.Length() < SlideThreshold)
+			{
+				PerformCrouch();
+			}
+		}
+		else if (_rig.IsCrouching() && _velocity.Length() > SlideThreshold)
+		{
+			Slide();
+		}
+		else
+		{
+			Player.FloorStopOnSlope = true;
+		}
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		SlideMovement(@event);
+
+		if (@event.IsActionPressed("crouch") && _velocity.Length() < SlideThreshold)
+		{
+			PerformCrouch();
+		}
+		else if (@event.IsActionPressed("crouch") && _velocity.Length() > SlideThreshold)
+		{
+			GD.Print("trying to slide at ", _velocity);
+			Slide();
+		}
+		else if (@event.IsActionReleased("crouch"))
+		{
+			Stand();
+		}
+	}
+
+	private void PerformCrouch()
+	{
+		_playerMesh.Scale = _crouchMeshScale;
+		var capsule = (CapsuleShape3D)_collisionShape.Shape;
+		_collisionShape.Position = new Vector3(_collisionShape.Position.X, 0.5f, _collisionShape.Position.Z);
+		capsule.Height = CrouchHeight;
+		_rig.Travel("Crouch");
+	}
+
+	private void Stand()
+	{
+		_playerMesh.Scale = _standMeshScale;
+		var capsule = (CapsuleShape3D)_collisionShape.Shape;
+		capsule.Height = StandHeight;
+		_collisionShape.Position = new Vector3(_collisionShape.Position.X, 1f, _collisionShape.Position.Z);
+		_rig.Travel("MoveSpace");
+	}
+
+	private void Slide()
+	{
+		if (_rig.IsSliding()) return;
+		if (_velocity.Length() > SlideThreshold)
+		{
+			_rig.Travel("Slide");
+		}
+	}
+
+	private void SlideMovement(InputEvent @event)
+	{
+		if (@event.IsActionPressed("move_left"))
+			_rotatePlayerLeft = true;
+		else if (@event.IsActionReleased("move_left"))
+			_rotatePlayerLeft = false;
+
+		if (@event.IsActionPressed("move_right"))
+			_rotatePlayerRight = true;
+		else if (@event.IsActionReleased("move_right"))
+			_rotatePlayerRight = false;
+	}
+
+	private void ApplySimpleSlide(float delta)
+	{
+		Player.FloorStopOnSlope = false;
+
+		if (_rotatePlayerLeft)
+			_rigPivot.Rotation = new Vector3(_rigPivot.Rotation.X, _rigPivot.Rotation.Y + SlideTurnRate, _rigPivot.Rotation.Z);
+		if (_rotatePlayerRight)
+			_rigPivot.Rotation = new Vector3(_rigPivot.Rotation.X, _rigPivot.Rotation.Y - SlideTurnRate, _rigPivot.Rotation.Z);
+
+		if (Player.IsOnFloor())
+		{
+			_velocity.X *= SlideFriction;
+			_velocity.Z *= SlideFriction;
+
+			Vector3 floorNormal = Player.GetFloorNormal();
+			float gravity = (float)Player.Call("get_player_gravity");
+			float steepness = 1.0f - floorNormal.Dot(Vector3.Up);
+			Vector3 slopeDir = (floorNormal * Vector3.Down.Dot(floorNormal) - Vector3.Down).Normalized();
+			Vector3 slopeAccel = slopeDir * gravity * delta * Mathf.Pow(steepness, 0.58f) * 10.0f;
+			_velocity += slopeAccel;
+
+			float horizontalSpeed = new Vector2(_velocity.X, _velocity.Z).Length();
+			Vector3 facingDir = -_rigPivot.GlobalTransform.Basis.Z.Normalized();
+			_velocity.X = facingDir.X * horizontalSpeed;
+			_velocity.Z = facingDir.Z * horizontalSpeed;
+		}
+
+		_velocity.Y += (float)Player.Call("get_player_gravity") * delta;
+		Player.Velocity = _velocity;
+	}
+
+	public void _on_player_velocity_current(Vector3 currentVelocity)  // TODO: once Player.cs is done converting to C#, rename from snake_case to PascalCase
+	{
+		_velocity = currentVelocity;
+	}
+}
