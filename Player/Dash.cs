c50962d4574a7a8cf5ca8bbb83f5a1475cf31bd5
Gjosti <stian_943@hotmail.com>
2026-02-15 00:20:29 +0100

    dash logic improved, can now change movement directions, if no drastic change in direction -> momentum/velocity is maintained

--- Player/Dash.cs
+++ Player/Dash.cs
@@ -6,6 +6,7 @@ public partial class Dash : Node3D
     [Export] public float DashCooldown { get; set; } = 0.5f;
     [Export] public int ExtraDashes { get; set; } = 1;
     [Export] public float DashSpeed { get; set; } = 20f;
+    [Export] public float DirectionChangeThreshold { get; set; } = 0.7f; // ~45 degrees
 
     private Timer _timer;
     private Rig _rig;
@@ -97,20 +98,62 @@ public partial class Dash : Node3D
 
     private async System.Threading.Tasks.Task ApplyDashMovement(Vector3 dashVelocity)
     {
-        float dashDuration = (float)_animationPlayer.GetAnimation("Dash").Length;
+        float dashDuration = GetDashAnimationDuration();
         SceneTreeTimer dashTimer = GetTree().CreateTimer(dashDuration);
         
         while (dashTimer.TimeLeft > 0)
         {
-            Vector3 currentHorizontalVelocity = new Vector3(Player.Velocity.X, 0, Player.Velocity.Z);
-            
-            // Only apply dash velocity if it's faster than current velocity
-            Vector3 finalVelocity = currentHorizontalVelocity.Length() > dashVelocity.Length() 
-                ? currentHorizontalVelocity 
-                : dashVelocity;
-            
+            Vector3 finalVelocity = CalculateDashVelocity(dashVelocity);
             Player.Velocity = new Vector3(finalVelocity.X, Player.Velocity.Y, finalVelocity.Z);
             await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
         }
+    }
+
+    private float GetDashAnimationDuration()
+    {
+        return (float)_animationPlayer.GetAnimation("Dash").Length;
+    }
+
+    private Vector3 CalculateDashVelocity(Vector3 dashVelocity)
+    {
+        Vector3 currentHorizontalVelocity = GetHorizontalVelocity();
+        
+        if (!ShouldCheckDirection(currentHorizontalVelocity, dashVelocity))
+        {
+            return dashVelocity;
+        }
+
+        float directionSimilarity = GetDirectionSimilarity(currentHorizontalVelocity, dashVelocity);
+        
+        return IsSignificantDirectionChange(directionSimilarity)
+            ? dashVelocity
+            : SnapToDashDirectionWithHigherSpeed(dashVelocity, currentHorizontalVelocity);
+    }
+
+    private Vector3 GetHorizontalVelocity()
+    {
+        return new Vector3(Player.Velocity.X, 0, Player.Velocity.Z);
+    }
+
+    private bool ShouldCheckDirection(Vector3 currentVelocity, Vector3 dashVelocity)
+    {
+        return currentVelocity.Length() > 0.1f && dashVelocity.Length() > 0.1f;
+    }
+
+    private float GetDirectionSimilarity(Vector3 currentVelocity, Vector3 dashVelocity)
+    {
+        return currentVelocity.Normalized().Dot(dashVelocity.Normalized());
+    }
+
+    private bool IsSignificantDirectionChange(float directionSimilarity)
+    {
+        return directionSimilarity < DirectionChangeThreshold;
+    }
+
+    private Vector3 SnapToDashDirectionWithHigherSpeed(Vector3 dashVelocity, Vector3 currentVelocity)
+    {
+        float currentSpeed = currentVelocity.Length();
+        float finalSpeed = Mathf.Max(currentSpeed, dashVelocity.Length());
+        return dashVelocity.Normalized() * finalSpeed;
     }
 }
