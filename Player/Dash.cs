19f496a9839d819c38f3be3cdd95a2a074aadd82
Gjosti <stian_943@hotmail.com>
2026-02-15 18:26:08 +0100

    dash cooldown/refresh has been simplified. only tied to a timer.

--- Player/Dash.cs
+++ Player/Dash.cs
@@ -7,7 +7,7 @@ public partial class Dash : Node3D
     [Export] public int ExtraDashes { get; set; } = 1;
     [Export] public float DashSpeed { get; set; } = 20f;
     [Export] public float DirectionChangeThreshold { get; set; } = 0.7f; // ~45 degrees
-    [Export] public float DashRefreshDelay { get; set; } = 0.5f;
+    [Export] public float DashRefreshDelay { get; set; } = 1f;
 
     private Timer _dashCooldownTimer;
     private Timer _dashRefreshTimer;
@@ -19,19 +19,15 @@ public partial class Dash : Node3D
     public override void _Ready()
     {
         _dashCooldownTimer = GetNode<Timer>("Timer");
+        _dashRefreshTimer = _dashCooldownTimer; // Reuse the same timer
         _availableDashes = ExtraDashes;
         
         if (Player != null)
         {
-            _rig = Player.GetNode<Rig>("RigPivot/Rig");
             _rigPivot = Player.GetNode<Node3D>("RigPivot");
-            _animationPlayer = Player.GetNode<AnimationPlayer>("RigPivot/Rig/CharacterRig/AnimationPlayer");
-            Player.StateChanged += OnPlayerStateChanged;
         }
         
-        _dashRefreshTimer = new Timer();
-        AddChild(_dashRefreshTimer);
-        _dashRefreshTimer.Timeout += RefreshDashes;
+        _dashCooldownTimer.Timeout += RefreshDashes;
     }
 
     public override void _UnhandledInput(InputEvent @event)
@@ -39,16 +35,6 @@ public partial class Dash : Node3D
         if (@event.IsActionPressed("dash"))
         {
             PerformDash();
-        }
-    }
-
-    private void OnPlayerStateChanged(PlayerState newState, PlayerState oldState)
-    {
-        // Refresh dashes when landing
-        if (oldState == PlayerState.Jumping && 
-            (newState == PlayerState.Idle || newState == PlayerState.Running))
-        {
-            RefreshDashes();
         }
     }
 
@@ -65,12 +51,6 @@ public partial class Dash : Node3D
         Player.SetState(PlayerState.Dashing);
         
         await ApplyDashMovement(dashDirection * DashSpeed);
-        
-        // Start refresh timer if dashes were used
-        if (_availableDashes < ExtraDashes)
-        {
-            _dashRefreshTimer.Start(DashRefreshDelay);
-        }
         
         // Return to idle or running based on current velocity
         Vector3 horizontalVel = new Vector3(Player.Velocity.X, 0, Player.Velocity.Z);
@@ -129,6 +109,5 @@ public partial class Dash : Node3D
     private void RefreshDashes()
     {
         _availableDashes = ExtraDashes;
-        _dashRefreshTimer.Stop();
     }
 }
